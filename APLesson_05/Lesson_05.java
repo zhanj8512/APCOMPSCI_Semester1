cb91cea2bf59ffc4543b53661bb8a309f9f29fdf
Zhan_Jason <ZhanJ8512@sduhsd.lan>
2016-10-14 12:12:15 -0700

    lesson 5

---
+++ APLesson_05/Lesson_05.java
@@ -0,0 +1,16 @@
+public class Lesson_05
+{
+	public static void main(String[]args)
+	{
+		int num = 3;
+		if (num == 3)
+		{
+			System.out.println("num equals 3!");
+		}
+		
+		if(num ==5)
+		{
+			System.out.println("num equals 5!");
+		}
+	}
+}
