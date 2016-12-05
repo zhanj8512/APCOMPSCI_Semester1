cd4e94c86696f28b38f3eeda95dc6c27bcf13f82
zhanj8512 <Jason@jasons-mbp.sduhsd.lan>
2016-12-05 11:15:10 -0800

    updated

--- AP_Lesson_08/underscores.java
+++ AP_Lesson_08/underscores.java
@@ -1,14 +1,20 @@
-<<<<<<< HEAD
-import java.util.
-=======
 import java.util.Scanner;
 public class underscores
 {
     public static void main(String[]args)
-    {
+    {  
         Scanner kb = new Scanner(System.in);
-        System.out.println("Please enter a sentence)"
+        System.out.println("Please enter a sentence:");
+        System.out.println(replace(kb.nextLine()));
         
-    }  
-}   
->>>>>>> origin/master
+    }
+    
+    public static String replace(String sent)
+    {
+        int index = sent.indexOf(" ");
+        if(index == -1)
+        return sent;
+        return replace(sent.substring(0, index) + "_" + sent.substring(index+1));
+        
+    }
+}    
