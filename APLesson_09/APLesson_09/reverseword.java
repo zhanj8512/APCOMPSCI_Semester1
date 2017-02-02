a5091ac09affd58c91d3b7891b7e1fabf90e7f62
zhanj8512 <Jason@jasons-mbp.sduhsd.lan>
2017-02-02 12:03:20 -0800

    df

--- APLesson_09/APLesson_09/reverseword.java
+++ APLesson_09/APLesson_09/reverseword.java
@@ -5,17 +5,17 @@ public class reverseword
 	{
 		Scanner kb = new Scanner(System.in);
 		String[] words = new String[5];
-		System.out.println("Enter 5 words: ");
+		System.out.println("please enter 5 words: ");
 		for(int i = 0; i< words.length; i++)
 		{
 			words[i] = kb.next();
 		}
-		System.out.println("In order...");
+		System.out.println("these are the words in order...");
 		for(String word : words)
 		{
 			System.out.println(word);
 		}
-		System.out.println("\nReversed...");
+		System.out.println("\nThese are the words in reverse...");
 		reverse(words);
 	}
 	public static void reverse(String[]words)
