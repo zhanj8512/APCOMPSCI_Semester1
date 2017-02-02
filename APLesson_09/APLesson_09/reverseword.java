f72c5fe9b57e3572a971e693ea3244d91805cd59
zhanj8512 <Jason@jasons-mbp.sduhsd.lan>
2017-02-02 11:53:00 -0800

    fixed reverseword
    dowifherw

--- APLesson_09/APLesson_09/reverseword.java
+++ APLesson_09/APLesson_09/reverseword.java
@@ -3,28 +3,26 @@ public class reverseword
 {
 	public static void main(String[]args)
 	{
-		String [] words = new String[5];
+		Scanner kb = new Scanner(System.in);
+		String[] words = new String[5];
+		System.out.println("Enter 5 words: ");
 		for(int i = 0; i< words.length; i++)
 		{
 			words[i] = kb.next();
 		}
-		
-		System.out.println("In order");
-		
+		System.out.println("In order...");
 		for(String word : words)
 		{
 			System.out.println(word);
 		}
-		
-		System.out.println(" ");
-		System.out.println("Reversed");
-		
-	 public static String reverse(String[] words)
+		System.out.println("\nReversed...");
+		reverse(words);
+	}
+	public static void reverse(String[]words)
+	{
+		for(int i = words.length; i > 0; i--)
 		{
-			for (int i = words.length; i >= 0; i--)
-			{
-				System.out.println(word);
-			}
+			System.out.println(words[i-1]);
 		}
 	}
 }
