be67e2e257a1cf4c46a52a0de26efb31a6ee3c5c
zhanj8512 <Jason@Jasons-MacBook-Pro.local>
2017-03-26 22:12:16 -0700

    new

--- AP_Lesson12/AP_Lesson13-1/console.java
+++ AP_Lesson12/AP_Lesson13-1/console.java
@@ -9,16 +9,13 @@ public abstract class console extends gamesystem {
 	public console(String p) {
 		super(p);
 	}
-	
 	public abstract String getPlatform();
-	
 	public abstract String getController();
-	
 	public int getSerialNo() {
 		return super.getSerialNo();
 	}
 	
 	public String toString() {
-		return "Platform: " + getPlatform() + "\nSerial #: " + super.getSerialNo() + "\nController: " + getController();
+		return "platform: " + getPlatform() + "\nSerial #: " + super.getSerialNo() + "\nController: " + getController();
 	}
 }
